d2a66147993e110f8fe01f1590a538065316ef82
Bjorn Borud <borud@lab5e.com>
2025-02-16 16:58:36 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "rm67162.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "pins_config.h"
+#include "esp_timer.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+
+extern "C" void app_main(void)
+{
+	gpio_config_t io_conf = {
+		.pin_bit_mask = (1ULL << PIN_LED), // Bit mask for the pin
+		.mode = GPIO_MODE_OUTPUT,
+		.pull_up_en = GPIO_PULLUP_DISABLE,
+		.pull_down_en = GPIO_PULLDOWN_DISABLE,
+		.intr_type = GPIO_INTR_DISABLE};
+	gpio_config(&io_conf);
+
+	gpio_set_level(PIN_LED, 1);
+
+	rm67162_init(); // amoled lcd initialization
+	lcd_setRotation(3);
+
+	lcd_fill(0, 0, 536, 240, 0x1111);
+}
